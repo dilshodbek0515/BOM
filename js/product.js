e173b5802f049d9c6fee90ab557fa38654b09c20
dilshod <dilshodx516@gmail.com>
2024-10-16 10:45:03 +0500

    BOM

--- js/product.js
+++ js/product.js
@@ -1,4 +1,5 @@
 const content = document.querySelector(".content")
+const review = document.querySelector(".review")
 const LINK = "https://dummyjson.com"
 
 
@@ -16,17 +17,35 @@ getData()
 function createContent(data) {
     console.log(data);
     content.innerHTML = `
-     <div>
+  <div>
+      <div class="large_content">
+          <div>
             <img class="detail_image" src=${data.images[0]} alt="">
-     </div>
-     <div class="item_content">
+          </div>
+          <div class="item_content">
             <p class="title">${data.title}</p>
             <p class="brend">${data.brand}</p>
             <p class="price">${data.price} USD</p>
             <p class="categorys">${data.category}</p>
             <p class="stock">${data.stock}</p>
             <p class="descrip">${data.description}</p>
+          </div>
      </div>
-    `
+  </div>
+`
+    data.reviews.forEach(item => {
+        const divEl = document.createElement("div")
+        divEl.className = "review_item"
+        divEl.innerHTML = `
+        <h3>${item.comment}</h3>
+        <p>${item.reviewerName}</p>
+        <div>
+        ${` <i class="fa-solid fa-star"></i>`.repeat(item.rating)}
+        ${` <i class="fa-regular fa-star"></i>`.repeat(5 - item.rating)}
+        </div >
+        `
+        review.appendChild(divEl)
+    });
 }
+
 
